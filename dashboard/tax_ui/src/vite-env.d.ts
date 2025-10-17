4304df0c8477ca1aea6a80c3e54cdbe6cee3f1d0
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-10-17 19:24:19 +0200

    dix

--- dashboard/tax_ui/src/vite-env.d.ts
+++ dashboard/tax_ui/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
     readonly VITE_DWANI_API_BASE_URL: string;
     readonly VITE_DWANI_API_KEY: string;
+    readonly VITE_UBERTAX_BACKEND_APP_API_URL: string;
   }
   
   interface ImportMeta {
