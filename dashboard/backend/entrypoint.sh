3d4e5548530e5932b5a95ac6dfd30b0efa72a43b
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-10-17 19:13:10 +0200

    test

--- dashboard/backend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run as root: Create data dir and set ownership only on the volume
-mkdir -p /app/data
-chown -R appuser:appuser /app/data
-
-# Switch to appuser and exec the command
-exec su-exec appuser "$@"
