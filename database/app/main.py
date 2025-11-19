ccea9facae23b7d668ec58470e468f008467517a
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-11-19 14:38:34 +0100

    add-fastapi-server

---
+++ database/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+#from app.api.v1 import legal_persons, natural_persons, graph
+#from app.core.tenant_middleware import tenant_middleware
+#from app.core.database import engine
+#from app.models import Base
+import asyncio
+
+app = FastAPI(title="Corporate Registry API")
+
+#app.add_middleware(tenant_middleware)
+
+#app.include_router(legal_persons.router, prefix="/legal_persons")
+#app.include_router(natural_persons.router, prefix="/natural_persons")
+#app.include_router(graph.router, prefix="/graph")
+
+'''
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        # Ensure common schema exists
+        await conn.execute("CREATE SCHEMA IF NOT EXISTS common")
+        await conn.run_sync(Base.metadata.create_all)  # creates tables in current schema
+'''
--- database/steps.md
+++ database/steps.md
@@ -1,8 +1,14 @@
 Steps for Implementation
 
 - Create Postgres DB
+    - Done
 - Create Tables
 - Insert Values in DB
+
+- Create fastpi server
+    - Done
+
+- Connect fastapi and postgres Container
 
 - Test Migration
 - Run 3 version of migrations
